1c3174832a1a0bb5d2a0cffecee3af2d32b263fe
18-xjh <535309586@qq.com>
2021-08-11 16:26:36 +0800

    界面设计和跳转

--- src/app.config.js
+++ src/app.config.js
@@ -2,10 +2,22 @@ export default {
   pages: [
     'pages/index/index'
   ],
+
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  subPackages:[
+    {
+      "root": "pages/test/",
+      "pages": [
+        'health',
+        'physique',
+        'physiqueTest'
+      ]
+    }
+
+  ]
 }
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,11 +1,13 @@
 import { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
-import { AtSearchBar } from 'taro-ui'
+import { View } from '@tarojs/components'
+import Taro from "@tarojs/taro";
+
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.less'
+
 
 @connect(({ counter }) => ({
   counter
@@ -31,16 +33,31 @@ class Index extends Component {
 
   componentDidHide () { }
 
-  render () {
-    return (
-      <View className='index'>
-        <AtSearchBar
-          placeholder="海哥一打五"
-        />
-      </View>
-    )
+  //转到体质辨识
+  toPhysique() {
+    Taro.navigateTo({url: '/pages/test/physique'})
   }
+
+  //转到健康管理规范
+  toHealth(){
+    Taro.navigateTo({url: '/pages/test/health'})
+  }
+render() {
+  return (
+    <View className='at-row  index'>
+      <View className='at-col at-col-6 index1'  onClick={this.toHealth.bind(this)}>
+        <View className='home-mid-knowledge'>
+          <text className='home-mid-know at-col--wrap'>中医健康管理知识宣教</text>
+        </View>
+      </View>
+      <View className='at-col at-col-6 index3' onClick={this.toPhysique.bind(this)}>
+        <View className='home-mid-identity'>
+          <text className='home-phy-ident'>【体质辨识】</text>
+          <text className='home-phy-ident-table'>判定量表</text>
+        </View>
+      </View>
+    </View>
+  );
 }
-
+}
 export default Index
-

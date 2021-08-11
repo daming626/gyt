6da3ef03f3e0d68fe49b6439df6a866810ac2c54
daming626 <74182133+daming626@users.noreply.github.com>
2021-08-11 23:03:36 +0800

    Update index.jsx

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -8,19 +8,8 @@ import {search} from "../../actions/search";
 
 import './index.less'
 
-@connect(() => ({
-}), (dispatch) => ({
-    search (keyword, condition, pageNum, pageSize) {
-        dispatch(search(keyword, condition, pageNum, pageSize))
-    },
-    getHospitals(page,condition) {
-        dispatch(getHospitals(page,condition))
-    }
-}))
 class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
+  componentWillReceiveProps () { }
 
   componentWillUnmount () { }
 
@@ -28,36 +17,10 @@ class Index extends Component {
 
   componentDidHide () { }
 
-  toIndexIconPage(target) {
-    switch (target) {
-        case GYT:
-            setTimeout(function () {
-                Taro.navigateTo({url: '/pages/hospital/hospitalGyt'});
-            }, 100);
-            break
-        case YYREGISTRATION:
-            this.props.search('', '', 1, 20)
-            setTimeout(function () {
-                Taro.navigateTo({url: '/pages/yyRegistration/yyhospital/index'});
-            }, 100);
-            break;
-        default:
-            break;
-    }
-}
 
   render () {
     return (
       <View className='index'>
-        <AtSearchBar
-          placeholder="海哥一打五"
-        />
-        <View onClick={this.toIndexIconPage.bind(this, YYREGISTRATION)}>
-        <Button>预约挂号</Button>
-        </View>
-        <View onClick={this.toIndexIconPage.bind(this, GYT)}>
-        <Button>国医堂</Button>
-        </View>
       </View>
     )
   }

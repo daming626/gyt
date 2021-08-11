17fe72ac97032c751dd309a254dca53e63269f04
19982594117 <846083037@qq.com>
2021-08-11 11:31:33 +0800

    预约挂号初步完成，还有小细节微调,删除测试文件，添加部分合并

--- src/app.config.js
+++ src/app.config.js
@@ -1,7 +1,6 @@
 export default {
   pages: [
     'pages/index/index',
-    'pages/test/index',
     'pages/yyRegistration/doctor/index',
     'pages/yyRegistration/yyhospital/index',
     'pages/hospital/hospitalGyt'
